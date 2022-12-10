e423dd1250846d1f7bff8a78ca5d3dc4eacfc968
Rajat U Krishna <krishna.ra@northeastern.edu>
2022-12-09 22:03:44 -0500

    pretty print stats

--- route-finder/utils/stats.py
+++ route-finder/utils/stats.py
@@ -9,3 +9,8 @@ class Stats:
 
     def endTime(self):
           self.totalTime = time.perf_counter() - self.startTime
+
+    def toPrettyString(self):
+        response = "Total time\tNodes expanded\tCost of route\n"
+        response += str(round(self.totalTime, 2)) + "\t\t" + str(self.nodeCount) + "\t\t" + str(self.totalCost)
+        return response
