1abeb868afd677359a4ef4676813ebee39068629
Rajat U Krishna <krishna.ra@northeastern.edu>
2022-12-10 19:44:48 -0500

    read parameters from config file

--- route-finder/route_finder.py
+++ route-finder/route_finder.py
@@ -2,13 +2,32 @@ from utils.cities import Cities
 from utils.state import SearchState
 from search import UniformCostSearch, MCTS
 import sys, getopt
+import yaml
+
+def readConfig(path : str):
+    with open(path, "r") as stream:
+        try:
+            return yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            print("Invalid config yaml!")
+            exit()
 
 if (__name__ == "__main__"):
+    # default search method
     searchMethod = 'UniformCostSearch'
-    logEnable = False
     
+    # load config
+    pathToConfig = "config/config.yml"
+    config = readConfig(pathToConfig)
+    
+    logEnable = config["logEnable"] if "logEnable" in config else False
+    searchArgs = {
+        "logEnable":logEnable
+        }
+
+    # read command line args
     try:
-        opts, args = getopt.getopt(sys.argv[1:],"ls:",["search=", "log"])
+        opts, args = getopt.getopt(sys.argv[1:],"s:",["search="])
     except getopt.GetoptError:
         print("route_finder.py -s <searchmethod>")
         sys.exit(2)
@@ -19,10 +38,19 @@ if (__name__ == "__main__"):
                 searchMethod = 'UniformCostSearch'
             elif arg == 'mcts':
                 searchMethod = 'MCTS'
-        elif opt == '--log' or opt == '-l':
-            logEnable = True
 
-    search = globals()[searchMethod](logEnable)
+                expWeight = config["mcts"]["explorationWeight"] if "mcts" in config and "explorationWeight" in config["mcts"]\
+                                 else 1
+                rewardWeight = config["mcts"]["rewardWeight"] if "mcts" in config and "rewardWeight" in config["mcts"]\
+                                 else 1
+                noIterationsRollout = config["mcts"]["noIterationsRollout"] if "mcts" in config and "noIterationsRollout" in config["mcts"]\
+                                 else 1
+
+                searchArgs['expWeight'] = expWeight
+                searchArgs['rewardWeight'] = rewardWeight
+                searchArgs['noIterationsRollout'] = noIterationsRollout
+
+    search = globals()[searchMethod](**searchArgs)
 
     # retrieve cities
     cities = Cities().getCities()
