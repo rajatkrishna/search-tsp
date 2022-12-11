816b42f9367bbc7f71cb9c65cd5db8207a752cf3
Rajat U Krishna <krishna.ra@northeastern.edu>
2022-12-11 00:44:54 -0500

    fix eq, hash and repr

--- route-finder/utils/node.py
+++ route-finder/utils/node.py
@@ -23,10 +23,13 @@ class Node:
 
         return self.state == other.state\
              and self.directions == other.directions\
-             and self.cost == other.cost
+             and round(self.cost, 5) == round(other.cost, 5)
 
     def __hash__(self):
-        return hash(self.state.__str__() + str(self.directions) + str(self.cost))
+        return hash(self.__str__())
 
     def __repr__(self):
-        return self.state.__str__()
+        return self.__str__()
+
+    def __str__(self):
+        return self.state.__str__() + str(self.directions) + str(round(self.cost, 5))
